449a270caa13ca1fba7b0b65d450ef703c556d82
ZurawskyX <renaldi.re28@gmail.com>
2018-10-14 02:24:31 +0700

    dropdown app menu

--- home/page/app.php
+++ home/page/app.php
@@ -1,72 +1,61 @@
-<style type="text/css">
-	.divApps
-	{
-		display: inline-block;
-	}
-	.pnlAppsBackground
-	{
-		font-size: 0px;
-	}	
-</style>
-
-<div class="pnlAppsBackground" id="mainPnlApp">
-	<?php
-		$query="SELECT * FROM apps WHERE isShown='true'";
-		$result=mysqli_query($con,$query);
-
-		while($row=mysqli_fetch_assoc($result))
-		{
-	?>
-			<div class="divApps" id="pnlApps" style="background-color: <?= $row['primaryColor'] ?>">
-				<p class="lblMyApplication" style="color: <?= $row['headerTextColor'] ?>">APPS</p>
-				<div class="preview-app" id="mainPreview-<?= $row['appsId'] ?>" style="background-image: url(<?= $row['path'].'1.png' ?>)">
-				</div>
-				<div class="detail-bottom" id="pnlMiniPreview">
-					<?php
-						for($i=1;$i<=$row['previewCount'];$i++)
-						{
-					?>
-							<div id="miniPreview-<?= $row['appsId'].'-'.$i ?>" class="mini-preview"
-								style="
-									background-image: url(<?= $row['path'].$i.'.png'?>);
-									<?php
-										if($i==1)
-										{
-									?>
-											border: 3px solid <?= $row['secondaryColor'] ?>;
-									<?php
-										}
-									?>
-								"
-								onclick="clickMiniPreview(<?= $row['appsId'].",".$i ?>)"></div>
-					<?php
-						}
-					?>
-				</div>
-				<a id="linkSite-<?= $row['appsId'] ?>" class="link" target="blank" href="<?= $row['linkSite'] ?>" 
-					style="
-						background-color: <?= $row['secondaryColor'] ?>;
-						color: <?= $row['textColor'] ?>">
-				Go to the Official Site!</a>
-				<div class="button-down2" onclick="scrolltoContact()"></div>
-			</div>
-	<?php
-		}
-	?>
-</div>
-<div class="appList">
-	<?php
-		$query="SELECT * FROM apps WHERE isShown='true'";
-		$result=mysqli_query($con,$query);
-
-        $i=1;
-		while($row=mysqli_fetch_assoc($result))
-		{
-	?>
-			<button class="apps" id="app-<?= $row['appsId'] ?>" onclick="showAppDetail(<?= $i ?>)">
-				<?= $row['appsName'] ?></button>
-	<?php
-	        $i++;
-		}
-	?>
+<style type="text/css">
+   .divApps
+   {
+   display: inline-block;
+   }
+   .pnlAppsBackground
+   {
+   font-size: 0px;
+   }	
+</style>
+<div class="pnlAppsBackground" id="mainPnlApp">
+   <?php
+      $query="SELECT * FROM apps WHERE isShown='true'";
+      
+      $result=mysqli_query($con,$query);
+      
+      
+      
+      while($row=mysqli_fetch_assoc($result))
+      
+      {
+      
+      ?>
+   <div class="divApps" id="pnlApps" style="background-color: <?= $row['primaryColor'] ?>">
+      <p class="lblMyApplication" style="color: <?= $row['headerTextColor'] ?>">APPS</p>
+      <div class="preview-app" id="mainPreview-<?= $row['appsId'] ?>" style="background-image: url(<?= $row['path'].'1.png' ?>)">
+      </div>
+      <div class="detail-bottom" id="pnlMiniPreview">
+        <?php
+            for($i=1;$i<=$row['previewCount'];$i++)
+            {
+        ?>
+		         <div id="miniPreview-<?= $row['appsId'].'-'.$i ?>" class="mini-preview"
+		            style="
+		            background-image: url(<?= $row['path'].$i.'.png'?>);
+		            <?php
+		               if($i==1)
+		               {
+		            ?>
+		            		border: 3px solid <?= $row['secondaryColor'] ?>;
+		            <?php
+		               }
+		            ?>"
+		            onclick="clickMiniPreview(<?= $row['appsId'].",".$i ?>)"></div>
+        <?php
+            }
+        ?>
+      </div>
+      <a id="linkSite-<?= $row['appsId'] ?>" class="link" target="blank" href="<?= $row['linkSite'] ?>" 
+         style="
+         background-color: <?= $row['secondaryColor'] ?>;
+         color: <?= $row['textColor'] ?>">
+      Go to the Official Site!</a>
+      <div class="button-down2" onclick="scrolltoContact()"></div>
+   </div>
+   <?php
+      }
+      
+      ?>
 </div>
+
--- home/script/script.js
+++ home/script/script.js
@@ -1,94 +1,184 @@
-var btnApps = document.getElementsByClassName('apps');
-btnApps[0].style.backgroundColor="#0D47A1";
-
-var currentPage=1;
-var max=7;
-
-function scrolltoSkill()
-{
-	$('html,body').animate({ scrollTop: $("#pnlSkill").offset().top-60 }, 800);
-	$('.navBar-content').css('border-top','3px solid #01579B');
-}
-
-function scrolltoHome()
-{
-	$('html,body').animate({ scrollTop: 0 }, 800);
-	$('.navBar-content').css('border-top','3px solid gray');
-}
-
-function scrolltoContact()
-{
-	$('html,body').animate({ scrollTop: $("#pnlContact").offset().top }, 800);
-	$('.navBar-content').css('border-top','3px solid rgba(100,100,100,0.75)');
-}
-
-function scrolltoApps()
-{
-	$('html,body').animate({ scrollTop: $("#mainPnlApp").offset().top-60 }, 800);
-	$('.navBar-content').css('border-top','3px solid #2196f3');
-}
-
-function clickMiniPreview(id,frame)
-{
-	$keyword='miniPreview-'+id+'-';
-	$("div[id*="+$keyword+"]").css('border','none');
-
-	$secondaryColor = $('#linkSite-'+id).css('background-color');
-	$('#miniPreview-'+id+'-'+frame).css('border','3px solid '+$secondaryColor);
-
-	$selectedPreview = $('#miniPreview-'+id+'-'+frame).css('background-image');
-	$('#mainPreview-'+id).css('background-image',$selectedPreview);
-}
-
-function showAppDetail(id)
-{
-	if(id>max || id<1) return;
-	$page=((id-currentPage+1)*101)-101;
-
-	$('#mainPnlApp').animate({'margin-left':'-='+$page+'vw'},1000);
-	currentPage=id;
-
-	for(i=0;i<btnApps.length;i++)
-	{
-		btnApps[i].style.backgroundColor="#2196F3";
-	}
-	btnApps[id-1].style.backgroundColor="#0D47A1";
-}
-
-function closeDimDetail()
-{
-	document.getElementById('dimDetail').style.visibility="hidden";
-}
-
-function showScreenshotDetail(id)
-{
-	var url=document.getElementById("ss"+id).style.backgroundImage;
-	if(url!="")
-	{
-		ssDetail.style.backgroundImage=url;
-		dimDetail.style.visibility="visible";
-	}
-}
-scrolltoHome();
-showAppDetail(1);
-$('html,body').animate({ scrollLeft: 0 }, 500);
-
-$('#lblIntro').animate({ "margin-top": "+=45vh" },700,function(){
-	setTimeout(function(){
-		$('#lblIntro').fadeOut(function(){
-			$('#lblIntro').html("Welcome!");
-			$('#lblIntro').fadeIn(1000,function(){
-				$('#lblIntroInstruction').fadeIn();
-				$('#introBg').click(function(){
-					$('#introBg').animate({ "margin-top": "-=100vh" },1000,function(){
-						$('#introBg').hide();
-						$("#lblName").fadeIn(3000);
-						$("#wrapDetail").fadeIn(3000,function(){
-							$('body').css('overflow-y','auto');
-						});
-					});
-				});
-			});
-		});
-	},2000);
+var currentPage = 1;
+
+var max = 7;
+
+$(window).click(function (e) {
+	if (e.target.id != 'liApps' && e.target.id!='btnToApps') {
+		$('.app-option').hide();
+	}
+	if(e.target.className=="app-option-menu")
+	{
+		$('.app-option-menu').removeClass('selected-app-option');
+		$(e.target).addClass('selected-app-option');
+	}
+})
+
+
+function scrolltoSkill()
+
+{
+
+	$('html,body').animate({
+		scrollTop: $("#pnlSkill").offset().top - 60
+	}, 800);
+
+	$('.navBar-content').css('border-top', '3px solid #01579B');
+
+}
+
+
+function scrolltoHome()
+
+{
+
+	$('html,body').animate({
+		scrollTop: 0
+	}, 800);
+
+	$('.navBar-content').css('border-top', '3px solid gray');
+
+}
+
+
+function scrolltoContact()
+
+{
+
+	$('html,body').animate({
+		scrollTop: $("#pnlContact").offset().top
+	}, 800);
+
+	$('.navBar-content').css('border-top', '3px solid rgba(100,100,100,0.75)');
+
+}
+
+
+function scrolltoApps()
+
+{
+
+	$('html,body').animate({
+		scrollTop: $("#mainPnlApp").offset().top - 60
+	}, 800);
+	$('.app-option').show();
+	$('.navBar-content').css('border-top', '3px solid #2196f3');
+
+}
+
+
+function clickMiniPreview(id, frame)
+
+{
+
+	$keyword = 'miniPreview-' + id + '-';
+
+	$("div[id*=" + $keyword + "]").css('border', 'none');
+
+
+	$secondaryColor = $('#linkSite-' + id).css('background-color');
+
+	$('#miniPreview-' + id + '-' + frame).css('border', '3px solid ' + $secondaryColor);
+
+
+	$selectedPreview = $('#miniPreview-' + id + '-' + frame).css('background-image');
+
+	$('#mainPreview-' + id).css('background-image', $selectedPreview);
+
+}
+
+
+function showAppDetail(id)
+
+{
+
+	if (id > max || id < 1) return;
+
+	$page = ((id - currentPage + 1) * 101) - 101;
+
+
+	$('#mainPnlApp').animate({
+		'margin-left': '-=' + $page + 'vw'
+	}, 1000);
+
+	currentPage = id;
+
+}
+
+
+function closeDimDetail()
+
+{
+
+	document.getElementById('dimDetail').style.visibility = "hidden";
+
+}
+
+
+function showScreenshotDetail(id)
+
+{
+
+	var url = document.getElementById("ss" + id).style.backgroundImage;
+
+	if (url != "")
+
+	{
+
+		ssDetail.style.backgroundImage = url;
+
+		dimDetail.style.visibility = "visible";
+
+	}
+
+}
+
+scrolltoHome();
+
+showAppDetail(1);
+
+$('html,body').animate({
+	scrollLeft: 0
+}, 500);
+
+
+$('#lblIntro').animate({
+	"margin-top": "+=45vh"
+}, 700, function () {
+
+	setTimeout(function () {
+
+		$('#lblIntro').fadeOut(function () {
+
+			$('#lblIntro').html("Welcome!");
+
+			$('#lblIntro').fadeIn(1000, function () {
+
+				$('#lblIntroInstruction').fadeIn();
+
+				$('#introBg').click(function () {
+
+					$('#introBg').animate({
+						"margin-top": "-=100vh"
+					}, 1000, function () {
+
+						$('#introBg').hide();
+
+						$("#lblName").fadeIn(3000);
+
+						$("#wrapDetail").fadeIn(3000, function () {
+
+							$('body').css('overflow-y', 'auto');
+
+						});
+
+					});
+
+				});
+
+			});
+
+		});
+
+	}, 2000);
+
 });
