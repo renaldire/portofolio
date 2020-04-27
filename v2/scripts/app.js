d0958e7ebdfa686614778aaec2c12d558d0c1aa6
Renaldi <renaldi.re28@gmail.com>
2020-04-27 14:41:56 +0700

    version 2: portofolio

--- v2/scripts/app.js
+++ v2/scripts/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$('.large-photo').hover(function(){
-		$(this).find('.opacity').css('opacity',0.5);
+		$(this).find('.opacity').css('opacity',0.8);
 	});
 	$('.large-photo').mouseleave(function(){
 		$(this).find('.opacity').css('opacity',0);
 	});
-})
+});
